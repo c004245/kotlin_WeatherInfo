02e6126e6313be21ac883837d2a5d18500f302c5
DESKTOP-E955KGF\hyunwook <c004245@naver.com>
2018-04-29 22:17:38 +0900

    데이터를 백그라운드에서 받아오는 AsyncTaskLoader,
    menu 기능추가

--- app/src/main/java/com/example/hyunwook/weathercast/ForecastDataLoader.kt
+++ app/src/main/java/com/example/hyunwook/weathercast/ForecastDataLoader.kt
@@ -2,8 +2,14 @@ package com.example.hyunwook.weathercast
 
 import android.content.Context
 import android.support.v4.content.AsyncTaskLoader
+import android.util.Log
 import com.example.hyunwook.weathercast.data.CityData
+import com.example.hyunwook.weathercast.data.DayData
 import com.example.hyunwook.weathercast.data.WeatherForecast
+import com.example.hyunwook.weathercast.data.WeekData
+import com.google.gson.Gson
+import java.io.InputStreamReader
+import java.net.URL
 
 /**
  * Created by hyunwook on 2018-04-29.
@@ -11,8 +17,37 @@ import com.example.hyunwook.weathercast.data.WeatherForecast
 class ForecastDataLoader(context: Context, val cities: ArrayList<CityData>) :
         AsyncTaskLoader<ArrayList<WeatherForecast>>(context) {
 
-    val API_KEY = String = "2677d0da8acab2b1d91d54942d6914c8"
-    val Currennt_URL  String = "http://api.openweathermap.org/data/2.5/weather?id="
-    val Forcast_URL
+    val API_KEY : String = "2677d0da8acab2b1d91d54942d6914c8"
+    val Current_URL : String = "http://api.openweathermap.org/data/2.5/weather?id="
+    val Forcast_URL : String = "http://api.openweathermap.org/data/2.5/forecast?id="
+    val ICON_URL : String = "http://openweathermap.org/img/w/"
+
+    override fun loadInBackground(): ArrayList<WeatherForecast> {
+        val city_weather = ArrayList<WeatherForecast>()
+        cities.forEach {
+            val cur_url = Current_URL+it._id+"&units=metric&APPID=$API_KEY"
+            val readData = URL(cur_url).readText()
+
+            val current: DayData = Gson().fromJson(readData, DayData::class.java)
+//            Log.d(TAG, "current ->" + current)
+
+            current.cityName = it.name
+            current.api_id = it._id
+
+            val fore_url = Forcast_URL+it._id+"&units=metric&APPID=$API_KEY"
+            val url = URL(fore_url)
+            val inputStream = InputStreamReader(url.openStream())
+
+            val week: WeekData = Gson().fromJson(inputStream, WeekData::class.java)
+            val forcast: WeatherForecast = WeatherForecast(current, week, ICON_URL)
+            city_weather.add(forcast)
+        }
+
+        return city_weather
+    }
+
+    companion object {
+//        val TAG = ForecastDataLoader.javaClass::getSimpleName
+    }
 
 }
